81abdad9c25335b01240d0771e647c248e772ac1
Jakubszadkowski <63921357+Jakubszadkowski@users.noreply.github.com>
2021-11-26 12:42:59 +0100

    Add files via upload

---
+++ find_valid_pesels.js
@@ -0,0 +1,106 @@
+function validate(num){
+
+    if (num.length!=11)
+        return false;
+    else{
+        var sum=0;
+        var wage=[1,3,7,9,1,3,7,9,1,3];
+        for (var i=0; i<10;i++)
+        {
+            sum+=wage[i]*num[i];
+        }
+        sum=sum%10;
+        if(sum==10) {
+            sum=0;
+        }
+        else{
+            sum=10-sum;
+        }
+        if(sum==num[10]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
+this.addEventListener('message',function(e){
+    switch(e.data){
+        case'stop':
+            //this.postMessage('watek zatrzymany');
+            this.close();
+            break;
+        default:
+            var arra=[];
+            var count=0;
+            for(var year=1930;year<=2050;year++)
+            {
+                
+                for(var month = 1; month<13;month++)
+                {
+                    
+                    for(var day = 1; day<32;day++)
+                    {
+                       
+                        if(day==31&&(month==4 || month==6 || month==9 || month==11))
+                        {   day=99;
+                            continue;
+                        }
+                        if(day>=29&&month==2&&year%4!=0)
+                            {                       
+                                day=99;   
+                                continue;
+
+                            }
+                        if(day>=30&&month==2&&year%4==0){
+                            continue;                
+                        }
+                        var year2 = "";
+                        var month2 = "";
+                        var day2 = "";
+                        
+                        if(year>=1800&&year<=1899)
+                            month2=80+month;
+                        if(year>=2000&&year<=2099)
+                            month2=20+month;
+
+                        
+                        year2=String(year).substring(2,4);
+
+                        
+                        if(String(month).length==1)
+                            month2="0"+String(month);
+                        else {
+                            month2=String(month);
+                        }
+
+                        
+                        if(String(day).length==1)
+                            day2="0"+String(day);
+
+                        else {
+                            day2=String(day);
+                        }
+
+                        if(validate(year2+""+month2+day2+e.data))
+                        {
+                                arra[count]=year2+""+month2+day2+e.data+"<BR/>";
+                                count++;
+                        }
+                            
+                        
+                    }
+                }
+            }
+            this.postMessage(arra);
+
+    }
+},false);
+
+
+// this.addEventListener('error', function(e) {
+//     alert('wystapil blad w linii: ' + e.lineno +
+//           ' w pliku: ' + e.filename + '.' +
+//           'Tresc bledu: ' + e.message);
+// }, false);
--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 //document.getElementById("valFromNumb").addEventListener("click", initPeselFromOther()); 
 
 function initOnePesel(){
+	clrear();
 	var day = document.getElementById("day").value;
 	var month = document.getElementById("month").value;
 	var year = document.getElementById("year").value;
 	var other = document.getElementById("other").value;
-	if(day==""||month==""||year==""||other=="")alert("Błąd danych! Sprawdz czy dane sa wpisane poprawnie");
+	if(day==""||month==""||year==""||other==""||year.length!=4)
+		alert("Błąd danych! Sprawdz czy dane sa wpisane poprawnie");
 	else{
 
 	if(year>=1800&&year<=1899)month=80+Number(month);
@@ -36,24 +38,43 @@ function initOnePesel(){
 }
 
 function initPeselFromDate(){
+	clrear();
 	var day = document.getElementById("day").value;
 	var month = document.getElementById("month").value;
 	var year = document.getElementById("year").value;
-	if(year>=1800&&year<=1899)month=80+Number(month);
-	if(year>=2000&&year<=2099)month=20+Number(month);
-	if(year>=2100&&year<=2199)month=40+Number(month);
-	if(year>=2200&&year<=2299)month=60+Number(month);
-	if(month.length==1)month='0'+month;
-	var beg = year.substring(2,4)+''+month+''+day;
-	var worker = new Worker("show_all_pesels.js");
-	worker.addEventListener('message', function(e) {
-		addElement("pesel","Numery pesel prawidłowe : "+e.data)
-	}, false);
-	worker.postMessage(beg);
-	worker.postMessage('stop')
+	if(day==""||month==""||year==""||year.length!=4)
+	{
+		alert("Blad danych, wprowadz ponownie!");
+	}
+	else{
+		if(year>=1800&&year<=1899)month=80+Number(month);
+		if(year>=2000&&year<=2099)month=20+Number(month);
+		if(year>=2100&&year<=2199)month=40+Number(month);
+		if(year>=2200&&year<=2299)month=60+Number(month);
+		if(month.length==1)month='0'+month;
+		var beg = year.substring(2,4)+''+month+''+day;
+		var worker = new Worker("show_all_pesels.js");
+		worker.addEventListener('message', function(e) {
+			addElement("pesel","Numery pesel prawidłowe : "+e.data)
+		}, false);
+		worker.postMessage(beg);
+		worker.postMessage('stop')
+	}
+
 }
 function initPeselFromOther(){
-	console.log("He?");
+	var other = document.getElementById("other").value;
+	if(other==""||other.length!=5)
+	alert("Podane dane są nieprawidłowe!");
+	else{
+		var worker = new Worker("find_valid_pesels.js");
+		worker.addEventListener('message', function(e) {
+			addElement("pesel","Numery pesel prawidłowe : "+"<br>"+e.data)
+		}, false);
+		worker.postMessage(other);
+		worker.postMessage('stop')
+		
+	}
 }
 function addElement(mydiv,msg){
 	clrear();
